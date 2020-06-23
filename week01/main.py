4512560d41015c9aa5033af81e657a33977071ba
zhaol <18615215588@qq.com>
2020-06-23 16:05:54 +0800

    add sth

--- week01/main.py
+++ week01/main.py
@@ -1,19 +1,54 @@
+import random
+import time
 import requests
 import bs4
+import lxml
+import pandas as pd
+
+def print_iterable(i : "Iterable"):
+    for v in i:
+        print(v)
+
+def get_url(url : str, headers : str) -> str:
+    time.sleep(5)
+    resp = requests.get(url, headers={"user-agent":headers}) #获取url信息
+    print("请求url：{}，请求头：{}".format(url, headers))
+    if resp.status_code != 200:
+        raise Exception("请求http出现错误，响应码为：{}，相应信息：{}".format(resp.status_code, resp.text))
+    return resp.text
+
 
 if __name__ == "__main__":
-    url = "https://movie.douban.com/top250"
+    urls = (f"https://movie.douban.com/top250?start={page * 25}&filter=" for page in range(0, 11))  #函数调用时，生成器表达式可以不加外括号
     #防止反爬程序返回418
-    user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36"
-    headers = {"user-agent":user_agent}
+    user_agent = [
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36",
+        "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+        "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0);",
+        "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)",
+        "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.6; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+        "Mozilla/5.0 (Windows NT 6.1; rv:2.0.1) Gecko/20100101 Firefox/4.0.1",
+        "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; en) Presto/2.8.131 Version/11.11",
+        "Opera/9.80 (Windows NT 6.1; U; en) Presto/2.8.131 Version/11.11",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_0) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Maxthon 2.0)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; TencentTraveler 4.0)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; The World)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; SE 2.X MetaSr 1.0; SE 2.X MetaSr 1.0; .NET CLR 2.0.50727; SE 2.X MetaSr 1.0)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; 360SE)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Avant Browser)",
+        "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)",
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.138 Safari/537.36",
+        "Mozilla/5.0 (X11; Linux x86_64; rv:76.0) Gecko/20100101 Firefox/76.0"
+    ]
+    headers = {"user-agent":random.choice(user_agent)}  #在user_agent中随机选择一个用户代理
 
-    resp = requests.get(url, headers=headers)
-    print(resp.status_code)
-    bs_info = bs4.BeautifulSoup(resp.text, "html.parser")
-    all_div_element = bs_info.find_all("div", {"class":"hd"})
-    print(type(all_div_element))  #all_div_element是bs4.element.ResultSet类型
-    for e in all_div_element:
-        print(type(e))
-    content = ((a.get("href"),a.find("span").text) for e in all_div_element for a in e.find_all('a'))
-    for href in content:
-        print(href)
+    #获取所有电影信息列表
+    movie_list = map(lambda url: get_url(url, random.choice(user_agent)), (url for url in urls)) #生成器表达式返回每个url，并用filter进行过滤
+    print_iterable(movie_list)
+    #movie_info_list = starmap(get_url, () for v)
+
