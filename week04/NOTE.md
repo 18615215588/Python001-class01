256c50eb7bd9b50e3f472c72db5739a87aafb4a7
stbird <18615215588@qq.com>
2020-07-20 01:00:10 +0800

    week04作业

--- week04/NOTE.md
+++ week04/NOTE.md
@@ -1,1 +1,27 @@
 学习笔记
+
+1.pandas基于numpy实现，可进行矢量运算，适合处理复杂的数据统计
+
+2.表格与DataFrame，数据输入输出，数据检查
+
+3.DataFrame的行索引：index；列索引：columns
+
+4.pandas可将数据永久保存，支持：HTML，CSV，JSON，Excel等格式
+
+5.df.to_csv()/read_csv()   //写入csv文件，读取csv文件
+  df.head()  //获取前n行数据
+  df.tail()  //获取尾部n行数据
+  df.shape   //数据规模
+  df.describe()   //输出DF的主要数据特征
+  
+6.获取数据：df[列名]，df[[多个列的列名]]
+
+7.to_excel/read_excel:从excel文件中获取存储数据
+
+8.drop()方法删除一个或多个行或列，df[新列名] = col_new_data
+
+9.loc[],iloc[]选择基于索引值的行和序列号的行，行与列关联选择
+
+10.数据遍历：iterrows(),itertuples(),iteritmes()
+
+11.Series数据结构：一维向量数据
---
+++ week04/SQL_to_pandas.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 20 00:06:38 2020
+
+@author: zhaol
+"""
+
+1. SELECT * FROM data;
+
+data
+ 
+2. SELECT * FROM data LIMIT 10;
+
+data.head(10)
+ 
+3. SELECT id FROM data;  //id 是 data 表的特定一列
+ 
+data['id'] 或 data.id
+
+4. SELECT COUNT(id) FROM data;
+
+data['id'].count() 或 data['id'].size
+ 
+5. SELECT * FROM data WHERE id<1000 AND age>30;
+
+data[data.id < 1000][data.age > 30] 或 data[data.age > 30][data.id < 1000]
+
+data[data['id'] < 1000][data['age'] > 30]
+ 
+6. SELECT id,COUNT(DISTINCT order_id) FROM table1 GROUP BY id;
+
+7. SELECT * FROM table1 t1 INNER JOIN table2 t2 ON t1.id = t2.id;
+
+pd.merge(t1, t2, on='id')
+ 
+8. SELECT * FROM table1 UNION SELECT * FROM table2;
+
+pd.concat([table1,table2],axis=0,ignore_index=True)
+ 
+9. DELETE FROM table1 WHERE id=10;
+
+newTable = table1[table1.id != 10]
+ 
+10. ALTER TABLE table1 DROP COLUMN column_name;
+
+table1.drop(column_name, axis=1, inplace=True)
